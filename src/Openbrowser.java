911c95c7ed07782af5ff08927d2d2685fe33f29c
kaflemahesh <mahesh.kafle@smarsh.com>
2021-08-16 23:55:58 +0545

    Minor Change

--- src/Openbrowser.java
+++ src/Openbrowser.java
@@ -5,7 +5,7 @@ public class Openbrowser {
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mahesh.kafle\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver(); // Launch Chrome
         driver.get("https://www.google.com/");
         String title = driver.getTitle();
