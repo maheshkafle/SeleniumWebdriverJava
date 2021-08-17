a2e422098afbe3bfb981791153edd7c0a5de097d
kaflemahesh <mahesh.kafle@smarsh.com>
2021-08-18 00:16:32 +0545

    Minor Change

--- src/RadioButton.java
+++ src/RadioButton.java
@@ -9,7 +9,7 @@ public class RadioButton {
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mahesh.kafle\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://seleniumpractise.blogspot.com/2016/08/how-to-automate-radio-button-in.html");
         List<WebElement> radio_btns = driver.findElements(By.xpath("//input[@name='lang' and @Type='radio']"));
