c5be5009345168d077d125dff47f75a63763103b
kaflemahesh <mahesh.kafle@smarsh.com>
2021-08-19 00:57:45 +0545

    Implemented major TestNG annotations used in test cycle

--- src/TestNg/TestNGBasics.java
+++ src/TestNg/TestNGBasics.java
@@ -1,7 +1,47 @@
 package TestNg;
 
-public class TestNGBasics {
-    public static void main(String[] args) {
+import org.testng.annotations.*;
 
+public class TestNGBasics {
+
+    //TestNG annotations
+    @BeforeSuite
+    public void setup(){
+        System.out.println("BeforeSuite--->");
+    }
+
+    @BeforeTest
+    public void launchbrowser(){
+        System.out.println("BeforeTest-->>");
+    }
+
+    @BeforeClass
+    public void login(){
+        System.out.println("BeforeClass-->>");
+    }
+
+    @BeforeMethod
+    public void enterUrl(){
+        System.out.println("BeforeMethod");
+    }
+
+    @Test
+    public void googleTestTitle(){
+        System.out.println("Test");
+    }
+
+    @AfterTest
+    public void deleteAllCookies(){
+        System.out.println("AfterTest");
+    }
+
+    @AfterClass
+    public void closeBrowser(){
+        System.out.println("AfterClass");
+    }
+
+    @AfterSuite
+    public void generateTestReport(){
+        System.out.println("AfterSuite");
     }
 }
