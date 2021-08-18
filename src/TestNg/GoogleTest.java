01a0034f215326f44c5f06258eb1a3ec65670d10
kaflemahesh <mahesh.kafle@smarsh.com>
2021-08-19 01:17:00 +0545

    Implemented TestNG annotations while trying to test Google Title

---
+++ src/TestNg/GoogleTest.java
@@ -0,0 +1,35 @@
+package TestNg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.lang.Thread;
+
+
+public class GoogleTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mahesh.kafle\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+        driver.get("http://google.com");
+    }
+
+    @Test
+    public void getTitleTest(){
+        String title = driver.getTitle();
+        System.out.println(title);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
