c16f568d770503532e15a240c5aaf5bac81fe11c
kaflemahesh <mahesh.kafle@smarsh.com>
2021-08-19 00:43:01 +0545

    Moved to SeleniumBasics Folder

--- src/SeleniumBasics/RadioButton.java
+++ src/SeleniumBasics/RadioButton.java
@@ -1,3 +1,5 @@
+package SeleniumBasics;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,7 +11,7 @@ public class RadioButton {
 
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mahesh.kafle\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://seleniumpractise.blogspot.com/2016/08/how-to-automate-radio-button-in.html");
         List<WebElement> radio_btns = driver.findElements(By.xpath("//input[@name='lang' and @Type='radio']"));
