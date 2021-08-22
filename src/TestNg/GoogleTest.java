e07ce29c5baba9d456354f7f3ce5c32d4b9bbb70
kaflemahesh <mahesh.kafle@smarsh.com>
2021-08-22 20:55:18 +0545

    Minor Change

--- src/TestNg/GoogleTest.java
+++ src/TestNg/GoogleTest.java
@@ -15,7 +15,7 @@ public class GoogleTest {
 
     @BeforeMethod
     public void setup() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mahesh.kafle\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         Thread.sleep(3000);
