4f6f8a988986dbe68e22fd574d1eaedfe83541cc
kaflemahesh <mahesh.kafle@smarsh.com>
2021-08-22 20:54:28 +0545

    Added assertion in GoogleTest

--- src/TestNg/GoogleTest.java
+++ src/TestNg/GoogleTest.java
@@ -2,6 +2,7 @@ package TestNg;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,7 +15,7 @@ public class GoogleTest {
 
     @BeforeMethod
     public void setup() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mahesh.kafle\\Downloads\\chromedriver_win32\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         Thread.sleep(3000);
@@ -23,8 +24,10 @@ public class GoogleTest {
 
     @Test
     public void getTitleTest(){
-        String title = driver.getTitle();
-        System.out.println(title);
+        String expectedTitle = "Google";
+        String ActualTitle = driver.getTitle();
+        // System.out.println(ActualTitle);
+        Assert.assertEquals(expectedTitle, ActualTitle);
     }
 
     @AfterMethod
