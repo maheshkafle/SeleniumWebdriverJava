109d8e6f29f6a077e4fc3c5429aa20590d7ce197
kaflemahesh <mahesh.kafle@smarsh.com>
2021-09-09 23:32:23 +0545

    Minor Change

--- src/TestNg/TestNGBasics.java
+++ src/TestNg/TestNGBasics.java
@@ -4,7 +4,21 @@ import org.testng.annotations.*;
 
 public class TestNGBasics {
 
-    //TestNG annotations
+    //TestNG annotations: Interview Question
+    // Order of Annotations
+    // 1. BeforeSuite
+    // 2. BeforeTest
+    // 3. BeforeClass
+    // 4. BeforeMethod
+    // 5. Test
+    // 6. After Method
+    // 7. AfterClass
+    // 8. AfterTest
+    // 9. AfterSuite
+    // We never use main() in TestNG project
+    // Annotations starting with @Before keyword are Prerequisites Annotations
+    // Annotations starting with @After keyword are Postrequisites Annotations
+    // Annotations starting with @Test keyword are Test Cases
     @BeforeSuite
     public void setup(){
         System.out.println("BeforeSuite--->");
@@ -33,14 +47,14 @@ public class TestNGBasics {
     @AfterMethod
     public void afterMethod(){ System.out.println("After Method"); }
 
-    @AfterTest
-    public void deleteAllCookies(){
-        System.out.println("AfterTest");
-    }
-
     @AfterClass
     public void closeBrowser(){
         System.out.println("AfterClass");
+    }
+
+    @AfterTest
+    public void deleteAllCookies(){
+        System.out.println("AfterTest");
     }
 
     @AfterSuite
