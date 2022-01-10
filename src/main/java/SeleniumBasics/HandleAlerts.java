48047f1644331d5faf9ffd416ab4923824a6a986
Mahesh Kafle <kafledarkhorse@gmail.com>
2022-01-10 16:14:48 +0400

    Added alert.dismiss() method to handle cancel operation in alrerts

--- src/main/java/SeleniumBasics/HandleAlerts.java
+++ src/main/java/SeleniumBasics/HandleAlerts.java
@@ -13,13 +13,17 @@ public class HandleAlerts {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/javascript_alerts");
-        WebElement alert_button = driver.findElement(By.xpath("//*[@id=\"content\"]/div/ul/li[1]/button"));
+        driver.manage().window().maximize();
+        //WebElement alert_button = driver.findElement(By.xpath("//button[@onclick='jsAlert()']"));
+        //WebElement alert_button = driver.findElement(By.cssSelector("button[onclick='jsConfirm()']"));
+        WebElement alert_button = driver.findElement(By.xpath("//button[@onclick='jsPrompt()']"));
         alert_button.click();
         Alert alert = driver.switchTo().alert();
         Thread.sleep(3000);
         String alertMessage = driver.switchTo().alert().getText();
         System.out.println("Alert Message : "+ alertMessage);
         alert.accept();
+        //alert.dismiss(); // Hint: after execution of line no 26 it will click on cancel button of alert box.
         Thread.sleep(5000);
         driver.quit();
     }
