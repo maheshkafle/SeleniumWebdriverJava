68a11b9b090947b7314ff4cf4cf473f14420b214
Mahesh Kafle <kafledarkhorse@gmail.com>
2022-01-10 16:49:57 +0400

    Minor Changes

--- src/main/java/SeleniumBasics/Openbrowser.java
+++ src/main/java/SeleniumBasics/Openbrowser.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Openbrowser {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(); // Launch Chrome
         driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
         String title = driver.getTitle();
         System.out.println(title);
         if(title.equals("Google")){
